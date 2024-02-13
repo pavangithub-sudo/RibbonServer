608f690dd1ec3a2e9bb0db1e8f4fbdd1d0b01233
Bharath <vaddibharath2000@gmail.com>
2024-02-13 15:13:08 +0530

    Ribbon Server

---
+++ src/main/java/com/dev/RibbonServerApplication.java
@@ -0,0 +1,15 @@
+package com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RibbonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonServerApplication.class, args);
+	}
+
+}
